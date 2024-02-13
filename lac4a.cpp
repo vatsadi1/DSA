d943a2b015ca3939e46d2a4ca59c5a69437c3208
Aditya Kumar Mishra <147153177+vatsadi1@users.noreply.github.com>
2024-02-13 13:20:57 +0530

    Add files via upload

---
+++ lac4a.cpp
@@ -0,0 +1,19 @@
+#include<iostream> 
+ using namespace std;
+int main()
+{
+    int side;
+    cin>>side;
+    if(side==3)
+{
+    cout<<"given figure is triangle";
+
+}
+    else
+
+    {
+        cout<<"no triangle";
+    }
+
+return 0;
+}
