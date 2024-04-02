7509d618f17b5307afa312faa7c4f6a6b1947ace
Aditya Kumar Mishra <147153177+vatsadi1@users.noreply.github.com>
2024-04-02 21:09:28 +0530

    Add files via upload

---
+++ lac4g.cpp
@@ -0,0 +1,15 @@
+#include<iostream> 
+ using namespace std;
+int main()
+{
+    for(int i=0;i<4;i=i+1)
+    {
+        cout<<"*";
+        for(int j=0;j<3;j=j+1)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+return 0;
+}
