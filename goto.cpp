2fc71db3423e575a61d0890efa12ddcdd5ea26f6
Aditya Kumar Mishra <147153177+vatsadi1@users.noreply.github.com>
2024-07-31 01:44:23 +0530

    Add files via upload

---
+++ goto.cpp
@@ -0,0 +1,22 @@
+#include<iostream> 
+ using namespace std;
+void printnumbers()
+{
+ int n=1;
+cout<<"n"<<" ";
+n++;
+if ( n <= 10)
+
+    goto labe1;
+}
+    int main()
+
+    {
+        printnumbers ();
+        return 0;
+    }
+
+
+
+
+ 
