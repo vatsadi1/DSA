e680d522254d9d5ff85d76c4a9c44fd3de352501
Aditya Kumar Mishra <147153177+vatsadi1@users.noreply.github.com>
2024-08-04 22:57:38 +0530

    Add files via upload

---
+++ localglobal.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int glo=6;
+void sum()
+{
+  int a;
+  cout<< glo;
+}
+int main(){
+    int glo=9;
+    glo=76;
+    int a=14,b=15;
+    float pi= 3.14;
+    char d ='v';
+    bool is_true =  true;
+    sum();
+    cout<<glo<< is_true;
+}
