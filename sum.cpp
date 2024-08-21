51f26b279892b15266c8ad099dc7e1e5b809b7c8
Aditya Kumar Mishra <147153177+vatsadi1@users.noreply.github.com>
2024-08-21 20:43:30 +0530

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a, b ;
+    cout << "enter the value of a";
+    cin>> a;
+    cout<< "enter the value of b";
+    cin >> b;
+    cout<<a + b; 
+    return 0;
+    
+}
+
+
+
+
