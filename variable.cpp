cf67037961b1b6dbe42f15f3d335761bdc44d8a2
Aditya Kumar Mishra <147153177+vatsadi1@users.noreply.github.com>
2024-08-25 00:51:26 +0530

    Add files via upload

---
+++ variable.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // int a=2;
+    // int b=5;
+    int a=14,b=15;
+    float pi= 3.14;
+    char d ='v';
+    cout<<"this is the variable formation in the c++.\n and rhe value of a is"<<a<<".\n the value of b is "<<b;
+    cout <<" \n the value of pi is "<<pi;
+    cout <<"\n the value of d is"<<d;
+    return 0;
+    }
