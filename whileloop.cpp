8f45b68f7013d755ea7473c5b29de2e05e4c3831
Aditya Kumar Mishra <147153177+vatsadi1@users.noreply.github.com>
2024-08-29 17:19:44 +0530

    Add files via upload

---
+++ whileloop.cpp
@@ -0,0 +1,13 @@
+ #include<iostream> 
+  using namespace std;
+ int main()
+ {
+    int i=1;
+    while(true)
+    {
+        cout<<"JALDI WAHA SA HATO"<<endl;
+        i++;
+    
+    }
+ return 0;
+ }
